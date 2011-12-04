86951b413fa0b0030ab4cb9c72ab2625945a0fc9
Anthony Liguori <aliguori@us.ibm.com>
2011-12-04 11:17:51 -0600

    qdev: don't access name through info
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>

--- hw/usb-ohci.c
+++ hw/usb-ohci.c
@@ -1775,7 +1775,7 @@ static int usb_ohci_init(OHCIState *ohci, DeviceState *dev,
     memory_region_init_io(&ohci->mem, &ohci_mem_ops, ohci, "ohci", 256);
     ohci->localmem_base = localmem_base;
 
-    ohci->name = qdev_get_info(dev)->name;
+    ohci->name = object_get_type(OBJECT(dev));
     usb_packet_init(&ohci->usb_packet);
 
     ohci->async_td = 0;
