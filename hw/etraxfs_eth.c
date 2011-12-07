0cb08e58a82c39b8777fbfaa171994718b391e94
Anthony Liguori <aliguori@us.ibm.com>
2011-12-07 16:16:14 -0600

    qdev: don't access type name directly
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>

--- hw/etraxfs_eth.c
+++ hw/etraxfs_eth.c
@@ -608,7 +608,7 @@ static int fs_eth_init(SysBusDevice *dev)
 
 	qemu_macaddr_default_if_unset(&s->conf.macaddr);
 	s->nic = qemu_new_nic(&net_etraxfs_info, &s->conf,
-			      dev->qdev.info->name, dev->qdev.id, s);
+			      object_get_type(OBJECT(s)), dev->qdev.id, s);
 	qemu_format_nic_info_str(&s->nic->nc, s->conf.macaddr.a);
 
 	tdk_init(&s->phy);
