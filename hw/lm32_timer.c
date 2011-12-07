98f396e37ae566d77ad320201523622831b8a46f
Anthony Liguori <aliguori@us.ibm.com>
2011-12-07 16:19:17 -0600

    fixup patch monkey bugs

--- hw/lm32_timer.c
+++ hw/lm32_timer.c
@@ -201,9 +201,7 @@ static const VMStateDescription vmstate_lm32_timer = {
 };
 
 static Property lm32_timer_properties[] = {
-    DEFINE_PROP_UINT32(,
-    "frequency", LM32TimerState, freq_hz, DEFAULT_FREQUENCY,
-    ),
+    DEFINE_PROP_UINT32("frequency", LM32TimerState, freq_hz, DEFAULT_FREQUENCY),
     DEFINE_PROP_END_OF_LIST(),
 };
 
