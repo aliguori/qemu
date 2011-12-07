def2136c4008804e90d10b9c77859b73bd3d20b5
Anthony Liguori <aliguori@us.ibm.com>
2011-12-07 16:18:57 -0600

    hack to fix marvell audio

--- hw/marvell_88w8618_audio.c
+++ hw/marvell_88w8618_audio.c
@@ -47,7 +47,7 @@ typedef struct mv88w8618_audio_state {
     uint32_t play_pos;
     uint32_t last_free;
     uint32_t clock_div;
-    DeviceState *wm;
+    void *wm;
 } mv88w8618_audio_state;
 
 static void mv88w8618_audio_callback(void *opaque, int free_out, int free_in)
