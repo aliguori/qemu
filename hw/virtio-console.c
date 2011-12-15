670a42ee07f14431bb07328e9d0a2d6ddbdf98bb
Anthony Liguori <aliguori@us.ibm.com>
2011-12-15 14:59:51 -0600

    virtio serial is doing something bad

--- hw/virtio-console.c
+++ hw/virtio-console.c
@@ -98,10 +98,9 @@ static void chr_event(void *opaque, int event)
 static int virtconsole_initfn(VirtIOSerialPort *port)
 {
     VirtConsole *vcon = DO_UPCAST(VirtConsole, port, port);
-    VirtIOSerialPortInfo *info = DO_UPCAST(VirtIOSerialPortInfo, qdev,
-                                           qdev_get_info(&vcon->port.dev));
+    VirtIOSerialPortClass *k = VIRTIO_SERIAL_PORT_GET_CLASS(port);
 
-    if (port->id == 0 && !info->is_console) {
+    if (port->id == 0 && !k->is_console) {
         error_report("Port number 0 on virtio-serial devices reserved for virtconsole devices for backward compatibility.");
         return -1;
     }
@@ -109,9 +108,13 @@ static int virtconsole_initfn(VirtIOSerialPort *port)
     if (vcon->chr) {
         qemu_chr_add_handlers(vcon->chr, chr_can_read, chr_read, chr_event,
                               vcon);
-        info->have_data = flush_buf;
-        info->guest_open = guest_open;
-        info->guest_close = guest_close;
+        /* FIXME: This is not right */
+        abort();
+        if (0) {
+            k->have_data = flush_buf;
+            k->guest_open = guest_open;
+            k->guest_close = guest_close;
+        }
     }
 
     return 0;
