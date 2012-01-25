ed9847a863ef017f1c691e8511a46e45b029e50f
Anthony Liguori <aliguori@us.ibm.com>
2012-01-25 17:58:32 -0600

    piix3: make PIIX3-xen a subclass of PIIX3
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>

--- hw/piix_pci.c
+++ hw/piix_pci.c
@@ -527,23 +527,14 @@ static TypeInfo piix3_info = {
 
 static void piix3_xen_class_init(ObjectClass *klass, void *data)
 {
-    DeviceClass *dc = DEVICE_CLASS(klass);
     PCIDeviceClass *k = PCI_DEVICE_CLASS(klass);
 
-    dc->desc        = "ISA bridge";
-    dc->vmsd        = &vmstate_piix3;
-    dc->no_user     = 1;
-    k->no_hotplug   = 1;
-    k->init         = piix3_initfn;
     k->config_write = piix3_write_config_xen;
-    k->vendor_id    = PCI_VENDOR_ID_INTEL;
-    k->device_id    = PCI_DEVICE_ID_INTEL_82371SB_0; // 82371SB PIIX3 PCI-to-ISA bridge (Step A1)
-    k->class_id     = PCI_CLASS_BRIDGE_ISA;
 };
 
 static TypeInfo piix3_xen_info = {
     .name          = "PIIX3-xen",
-    .parent        = TYPE_PCI_DEVICE,
+    .parent        = "PIIX3",
     .instance_size = sizeof(PIIX3State),
     .class_init    = piix3_xen_class_init,
 };
