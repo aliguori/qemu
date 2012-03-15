63861ca6f96e83b9422d322960e9914dc993178a
Paolo Bonzini <pbonzini@redhat.com>
2012-03-15 10:19:57 +0100

    cmdline: reindent numa_add
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- vl.c
+++ vl.c
@@ -986,44 +986,44 @@ static void numa_add(const char *optarg)
     value = endvalue = 0ULL;
 
     optarg = get_opt_name(option, 128, optarg, ',') + 1;
-    if (!strcmp(option, "node")) {
-        if (get_param_value(option, 128, "nodeid", optarg) == 0) {
-            nodenr = nb_numa_nodes;
-        } else {
-            nodenr = strtoull(option, NULL, 10);
-        }
-
-        if (get_param_value(option, 128, "mem", optarg) == 0) {
-            node_mem[nodenr] = 0;
-        } else {
-            int64_t sval;
-            sval = strtosz(option, &endptr);
-            if (sval < 0 || *endptr) {
-                fprintf(stderr, "qemu: invalid numa mem size: %s\n", optarg);
-                exit(1);
-            }
-            node_mem[nodenr] = sval;
-        }
-        if (get_param_value(option, 128, "cpus", optarg) != 0) {
-            value = strtoull(option, &endptr, 10);
-            if (*endptr == '-') {
-                endvalue = strtoull(endptr+1, &endptr, 10);
-            } else {
-                endvalue = value;
-            }
-
-            if (!(endvalue < MAX_CPUMASK_BITS)) {
-                endvalue = MAX_CPUMASK_BITS - 1;
-                fprintf(stderr,
-                    "A max of %d CPUs are supported in a guest\n",
-                     MAX_CPUMASK_BITS);
-            }
-
-            bitmap_set(node_cpumask[nodenr], value, endvalue-value+1);
-        }
-        nb_numa_nodes++;
+    if (strcmp(option, "node")) {
+        return;
     }
-    return;
+    if (get_param_value(option, 128, "nodeid", optarg) == 0) {
+        nodenr = nb_numa_nodes;
+    } else {
+        nodenr = strtoull(option, NULL, 10);
+    }
+
+    if (get_param_value(option, 128, "mem", optarg) == 0) {
+        node_mem[nodenr] = 0;
+    } else {
+        int64_t sval;
+        sval = strtosz(option, &endptr);
+        if (sval < 0 || *endptr) {
+            fprintf(stderr, "qemu: invalid numa mem size: %s\n", optarg);
+            exit(1);
+        }
+        node_mem[nodenr] = sval;
+    }
+    if (get_param_value(option, 128, "cpus", optarg) != 0) {
+        value = strtoull(option, &endptr, 10);
+        if (*endptr == '-') {
+            endvalue = strtoull(endptr+1, &endptr, 10);
+        } else {
+            endvalue = value;
+        }
+
+        if (!(endvalue < MAX_CPUMASK_BITS)) {
+            endvalue = MAX_CPUMASK_BITS - 1;
+            fprintf(stderr,
+                "A max of %d CPUs are supported in a guest\n",
+                 MAX_CPUMASK_BITS);
+        }
+
+        bitmap_set(node_cpumask[nodenr], value, endvalue-value+1);
+    }
+    nb_numa_nodes++;
 }
 
 static int smp_init_func(QemuOpts *opts, void *opaque)
