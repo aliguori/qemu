03ae9ef9f8bff975024adef37337b5603aeed8ba
Paolo Bonzini <pbonzini@redhat.com>
2012-03-28 09:28:31 +0200

    QemuOpts: correctly skip escaped ,id=
    QemuOpts would parse incorrectly something that included ",,id=" (with
    an escaped comma).  Fix this by using get_param_value.
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- qemu-option.c
+++ qemu-option.c
@@ -889,18 +889,13 @@ static QemuOpts *opts_parse(QemuOptsList *list, const char *params,
 {
     const char *firstname;
     char value[1024], *id = NULL;
-    const char *p;
     QemuOpts *opts;
     Error *local_err = NULL;
 
     assert(!permit_abbrev || list->implied_opt_name);
     firstname = permit_abbrev ? list->implied_opt_name : NULL;
 
-    if (strncmp(params, "id=", 3) == 0) {
-        get_opt_value(value, sizeof(value), params+3);
-        id = value;
-    } else if ((p = strstr(params, ",id=")) != NULL) {
-        get_opt_value(value, sizeof(value), p+4);
+    if (get_param_value(value, sizeof(value), "id", params)) {
         id = value;
     }
     if (defaults) {
